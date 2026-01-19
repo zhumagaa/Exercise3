a1897142f987fc68b5350672d5dc4dce725d8351
zhumagaa <zhumagaa@union.edu>
2026-01-18 23:05:30 -0500

    Exercise 3A extension

--- main/main.c
+++ main/main.c
@@ -3,28 +3,36 @@
 
 
 #define LED_PIN GPIO_NUM_14        // LED pin
-#define BUTTON_PIN GPIO_NUM_4      // button pin
+#define BUTTON_PIN GPIO_NUM_4      // button pin 2
+#define BUTTON_PIN2 GPIO_NUM_5      // button pin 2, "enable button"
 
 bool bstatememory = false;   // Button state (true = pressed, false = not pressed)
 bool pressed;          // Flag to indicate a button press event
+bool enableActive;
 bool lstate = false;   // LED state (false = off, true = on)
 int pressCount = 0;     //counter to turn LED every otehr time (every second press)
 
 void app_main(void) {
-    // TO-DO: Configure LED output
+    // Configure LED output
     gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
     gpio_intr_disable(LED_PIN);
     gpio_set_level(LED_PIN, 0);
 
-    // TO-DO: Configure BUTTON input
+    // Configure BUTTON 1 input
+    gpio_reset_pin(BUTTON_PIN);
+    gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_en(BUTTON_PIN);
+
+    // Configure BUTTON 2 input
     gpio_reset_pin(BUTTON_PIN);
     gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
     gpio_pullup_en(BUTTON_PIN);
 
     while (1) {
         // TO-DO: Implement LED toggle and button logic here
-        pressed = gpio_get_level(BUTTON_PIN) == 0;     // Input active low button
+        pressed = gpio_get_level(BUTTON_PIN) == 0;     // active low button
+        enableActive = (gpio_get_level(BUTTON_PIN2) == 0); //active low as well
 
         if (!bstatememory && pressed) {            // Released button, is now pressed
             bstatememory = true;                   // Remember button is pressed  
@@ -34,14 +42,17 @@ void app_main(void) {
         if (bstatememory && !pressed) {            // Pressed button, is now released
             vTaskDelay(25 / portTICK_PERIOD_MS); // button debounce delay
             
-            if (gpio_get_level(BUTTON_PIN) == 1) {
-                pressCount++;
+            if (gpio_get_level(BUTTON_PIN)) {
                 //LED turns on every other (every second) time
                 // Client has to press button twice every time to turn the LED on/off
 
-                if (pressCount == 2) {
-                    lstate = !lstate;
-                    pressCount = 0;
+                if (gpio_get_level(BUTTON_PIN2) == 0) {
+                    //enable - state switches only if enable is active
+                    pressCount++;
+                    if (pressCount == 2) {
+                        lstate = !lstate;
+                        pressCount = 0;
+                    }
                 }
             }
             bstatememory = false;
@@ -49,6 +60,6 @@ void app_main(void) {
         
         gpio_set_level(LED_PIN, lstate);          // Output LED state
 
-        vTaskDelay(5 / portTICK_PERIOD_MS);      // loop delay
+        vTaskDelay(10 / portTICK_PERIOD_MS);      // loop delay
    }
 }
